7ddd15e690add2b03c376c2fd2a5bc079c7154f6
Bruno Diaz <56422761+crc8109@users.noreply.github.com>
2022-12-19 21:54:49 -0800

    Add VPC and subnet resource blocks

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,15 @@
 provider "aws" {
   region     = "us-west-1"
-  access_key = ""
-  secret_key = ""
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_vpc" "development-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+  vpc_id = aws_vpc.development-vpc.id
+  cidr_block = "10.0.10.0/24"
+  availability_zone = "us-west-1a"
 }
