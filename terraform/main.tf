0204057927db40e204c649db7ac217c248da3524
Bruno Diaz <56422761+crc8109@users.noreply.github.com>
2022-12-21 10:42:14 -0800

    Create subnet from data block and exisiting VPC

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,13 @@ resource "aws_subnet" "dev-subnet-1" {
   cidr_block        = "10.0.10.0/24"
   availability_zone = "us-west-1b"
 }
+
+data "aws_vpc" "exisiting_vpc" {
+  default = true
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+  vpc_id            = data.aws_vpc.exisiting_vpc.id
+  cidr_block        = "172.31.48.0/20"
+  availability_zone = "us-west-1c"
+}
