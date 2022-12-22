3206b843c09a7dc2263bbba942eede393222d4d4
Bruno Diaz <56422761+crc8109@users.noreply.github.com>
2022-12-21 17:39:55 -0800

    Add tags to resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,12 +6,18 @@ provider "aws" {
 
 resource "aws_vpc" "development-vpc" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "development"
+  }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
   vpc_id            = aws_vpc.development-vpc.id
   cidr_block        = "10.0.10.0/24"
   availability_zone = "us-west-1b"
+  tags = {
+    Name = "subnet-1-dev"
+  }
 }
 
 data "aws_vpc" "exisiting_vpc" {
@@ -22,4 +28,7 @@ resource "aws_subnet" "dev-subnet-2" {
   vpc_id            = data.aws_vpc.exisiting_vpc.id
   cidr_block        = "172.31.48.0/20"
   availability_zone = "us-west-1c"
+  tags = {
+    Name = "subnet-2-default"
+  }
 }
