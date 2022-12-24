1b1f66e47ff3ec0a554b00de1cbf4b8da992a587
Bruno Diaz <56422761+crc8109@users.noreply.github.com>
2022-12-23 17:39:25 -0800

    Create variable block for subnet's cidr block

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,8 @@ variable "secret_key" {
   type        = string
   description = "Secret Key for AWS account"
 }
+
+variable "subnet_cidr_block" {
+  description = "subnet cidr block"
+  default     = "10.0.40.0/24"
+}
