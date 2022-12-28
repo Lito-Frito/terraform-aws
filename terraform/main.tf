d5f893a78506d678ae6de5f320d44a89a3b5cc81
Bruno Diaz <56422761+crc8109@users.noreply.github.com>
2022-12-28 13:28:57 -0800

    Create VPC and subnet resources & rm other resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,39 +4,18 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-resource "aws_vpc" "development-vpc" {
-  cidr_block = var.cidr_blocks[0]
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_blocks
   tags = {
-    Name = var.environment
+    Name = "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id            = aws_vpc.development-vpc.id
-  cidr_block        = var.cidr_blocks[1]
-  availability_zone = "us-west-1b"
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_blocks
+  availability_zone = var.avail_zone
   tags = {
-    Name = "subnet-1-dev"
+    Name = "${var.env_prefix}-subnet-1"
   }
-}
-
-data "aws_vpc" "exisiting_vpc" {
-  default = true
-}
-
-resource "aws_subnet" "dev-subnet-2" {
-  vpc_id            = data.aws_vpc.exisiting_vpc.id
-  cidr_block        = var.cidr_blocks[2]
-  availability_zone = "us-west-1c"
-  tags = {
-    Name = "subnet-2-default"
-  }
-}
-
-output "dev-vpc-id" {
-  value = aws_vpc.development-vpc.id
-}
-
-output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
 }
