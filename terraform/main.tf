e5a4b936626dfb7352f329b923b37424a8f9d340
Bruno Diaz <56422761+crc8109@users.noreply.github.com>
2022-12-29 14:57:02 -0800

    Create security group resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,3 +38,34 @@ resource "aws_default_route_table" "default-rtb" {
     "Name" = "${var.env_prefix}-rtb"
   }
 }
+
+resource "aws_security_group" "myapp-sg" {
+  name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    "Name" = "${var.env_prefix}-sg"
+  }
+}
