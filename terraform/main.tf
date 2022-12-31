11c0cb98491045baf5217a562e2a0ca100793ca2
Bruno Diaz <56422761+crc8109@users.noreply.github.com>
2022-12-30 22:30:51 -0800

    Add output block for ip address of EC2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,6 +82,10 @@ data "aws_ami" "latest-amazon-linux-img" {
   }
 }
 
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
 resource "aws_key_pair" "ssh-key" {
   key_name   = "server-key"
   public_key = var.public_key
