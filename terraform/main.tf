448b4b5a2c8a47418195bb1d16975c084a924b09
Bruno Diaz <56422761+crc8109@users.noreply.github.com>
2023-01-03 14:11:58 -0800

    Add local-exec Provisoner to copy ec2 IP to output.txt

--- terraform/main.tf
+++ terraform/main.tf
@@ -129,6 +129,8 @@ resource "aws_instance" "myapp-server" {
     script = file("entry-script-on-ec2.sh")
   }
 
-  
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > output.txt"
+  }
 }
 
