6187aaf3ec67ec3900a24940b883489860611e2f
Bruno Diaz <56422761+crc8109@users.noreply.github.com>
2023-01-04 09:15:58 -0800

    Remove Docker Run to fix TF stuck in "Still Creating..."

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -2,4 +2,5 @@
 sudo yum update -y && sudo yum install -y docker
 sudo systemctl start docker
 sudo usermod -aG docker ec2-user
-docker run -p 8080:80 nginx
+# Need to comment out below because running a web server in remote-exec causes EC2 to be stuck in "Still Creating...""
+# docker run -p 8080:80 nginx
