5eacc86cd56032c9d928f49396108ec9b1ca9645
Bruno Diaz <56422761+crc8109@users.noreply.github.com>
2023-01-06 11:21:40 -0800

    Instantiate subnet module within main config file

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,34 +11,15 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id            = aws_vpc.myapp-vpc.id
-  cidr_block        = var.subnet_cidr_blocks
-  availability_zone = var.avail_zone
-  tags = {
-    Name = "${var.env_prefix}-subnet-1"
-  }
-}
 
-resource "aws_internet_gateway" "myapp-igw" {
+module "myapp-subnet" {
+  source = "./modules/subnet"
+  subnet_cidr_blocks = var.subnet_cidr_blocks
+  avail_zone = var.avail_zone
+  env_prefix = var.env_prefix
   vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    "Name" = "${var.env_prefix}-igw"
-  }
-}
-
-resource "aws_default_route_table" "default-rtb" {
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    "Name" = "${var.env_prefix}-rtb"
-  }
 }
-
 resource "aws_default_security_group" "default-sg" {
   vpc_id = aws_vpc.myapp-vpc.id
 
