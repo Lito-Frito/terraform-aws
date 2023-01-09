6ffd3ecb35be8c572d45536a692966aed19821a3
Bruno Diaz <56422761+crc8109@users.noreply.github.com>
2023-01-09 12:15:16 -0800

    Remove myapp-subnet Module and use AWS VPC Module

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,27 +4,30 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-resource "aws_vpc" "myapp-vpc" {
-  cidr_block = var.vpc_cidr_blocks
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = var.vpc_cidr_blocks
+
+  azs            = [var.avail_zone]
+  public_subnets = [var.subnet_cidr_blocks]
+  public_subnet_tags = {
+    Name = "${var.env_prefix}-subnet-1"
+  }
+
   tags = {
     Name = "${var.env_prefix}-vpc"
   }
 }
 
 
-module "myapp-subnet" {
-  source                 = "./modules/subnet"
-  subnet_cidr_blocks     = var.subnet_cidr_blocks
-  avail_zone             = var.avail_zone
-  env_prefix             = var.env_prefix
-  vpc_id                 = aws_vpc.myapp-vpc.id
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-}
+
 
 module "myapp-webserver" {
   source = "./modules/webserver"
 
-  vpc_id = aws_vpc.myapp-vpc.id
+  vpc_id = module.vpc.vpc_id
 
   my_ip = var.my_ip
 
@@ -36,7 +39,7 @@ module "myapp-webserver" {
 
   instance_type = var.instance_type
 
-  subnet_id = module.myapp-subnet.subnet.id
+  subnet_id = module.vpc.public_subnets[0]
 
   avail_zone = var.avail_zone
 }
