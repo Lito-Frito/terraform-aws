312f858ab9add2bac597118665811f0f096308ef
Bruno Diaz <56422761+crc8109@users.noreply.github.com>
2023-01-09 10:16:14 -0800

    Move entry script to webserver dir

--- terraform/modules/webserver/entry-script.sh
+++ terraform/modules/webserver/entry-script.sh
@@ -3,4 +3,4 @@ sudo yum update -y && sudo yum install -y docker
 sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 # Need to comment out below because running a web server in remote-exec causes EC2 to be stuck in "Still Creating...""
-# docker run -p 8080:80 nginx
+docker run -p 8080:80 nginx
