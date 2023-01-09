afba41493d58f9a8a1dcebceb27095aee71a08bf
Bruno Diaz <56422761+crc8109@users.noreply.github.com>
2023-01-09 10:12:55 -0800

    Move resources to module dir & create module block

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,97 +20,23 @@ module "myapp-subnet" {
   vpc_id                 = aws_vpc.myapp-vpc.id
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
-resource "aws_default_security_group" "default-sg" {
+
+module "myapp-webserver" {
+  source = "./modules/webserver"
+
   vpc_id = aws_vpc.myapp-vpc.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
-  }
+  my_ip = var.my_ip
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  env_prefix = var.env_prefix
 
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = []
-  }
+  image_name = var.image_name
 
-  tags = {
-    "Name" = "${var.env_prefix}-sg"
-  }
-}
+  public_key_location = var.public_key_location
 
-data "aws_ami" "latest-amazon-linux-img" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel*x86_64-gp2"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "server-key"
-  public_key = file(var.public_key_location)
-}
-
-resource "aws_instance" "myapp-server" {
-  # Required
-  ami           = data.aws_ami.latest-amazon-linux-img.id
   instance_type = var.instance_type
 
-  # Optional
-  subnet_id                   = module.myapp-subnet.subnet.id
-  vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
-  availability_zone           = var.avail_zone
-  associate_public_ip_address = true
-  key_name                    = aws_key_pair.ssh-key.key_name
+  subnet_id = module.myapp-subnet.subnet.id
 
-  tags = {
-    "Name" = "${var.env_prefix}-server"
-  }
-
-  # Ensure that EC2 is replaced when script is updated
-  # user_data_replace_on_change = true
-
-  # Script to execute
-  # user_data = file("entry-script.sh")
-
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ec2-user"
-    private_key = file(var.private_key_location)
-  }
-
-  provisioner "file" {
-    source      = "entry-script.sh"
-    destination = var.entry_script_location
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo chmod +x ${var.entry_script_location}",
-      "sudo sh ${var.entry_script_location}"
-    ]
-  }
-
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} > output.txt"
-  }
+  avail_zone = var.avail_zone
 }
-
